bb412df0dadf40d88b31434d1145664e1f9fc88e
eric <849269614@qq.com>
2020-07-08 23:17:59 +0800

    初始化

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="Login">
+    Login
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    components: {},
+    methods: {},
+    mounted() {
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
